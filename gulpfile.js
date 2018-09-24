80de46a8b9c89bfba01b8ce161c04de548f23858
marcio <marcio.hsdl@gmail.com>
2018-09-24 17:56:40 -0300

    [master] | lorem ipsum | gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -3,16 +3,33 @@ var csso = require('gulp-csso');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var concat = require('gulp-concat');
+var strip  = require('gulp-strip-css-comments');
+var clean = require('gulp-clean');
+var runSequence = require('run-sequence');
+
+gulp.task('clean', function () {
+  return gulp.src('./dist/*').pipe(clean());
+});
 
 gulp.task('styles', function(){
-  return gulp.src('assets/css/*.css')
+  return gulp.src(
+    [
+      'assets/css/*.css', 
+      'node_modules/@fortawesome/fontawesome-free/css/all.css'
+    ])
     .pipe(csso())
     .pipe(rename({
       suffix: '.min'
     }))
     .pipe(concat('all.css'))
-    .pipe(gulp.dest('gulp'))
+    .pipe(strip({preserve: false}))
+    .pipe(gulp.dest('gulp/css'))
 });
+
+gulp.task('fonts', function() {
+  return gulp.src('node_modules/@fortawesome/fontawesome-free/webfonts/*.*')
+    .pipe(gulp.dest('gulp/webfonts/'));
+})
 
 gulp.task('scripts', function(){
   return gulp.src('assets/js/*.js')
@@ -21,7 +38,9 @@ gulp.task('scripts', function(){
       suffix: '.min'
     }))
     .pipe(concat('all.js'))
-    .pipe(gulp.dest('gulp'))
+    .pipe(gulp.dest('gulp/js'))
 });
 
-gulp.task('default', [ 'styles', 'scripts' ]);
+gulp.task('default', function (cb) {
+  return runSequence('clean', [ 'styles', 'scripts', 'fonts' ], cb);
+});
