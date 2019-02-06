6e7d063384fb740c87c9cde17577bf8994298c4b
Onnion <marcio.hsdl@gmail.com>
2019-02-06 01:26:58 -0300

    [master] | new address

--- src/app/components/maps/maps.component.ts
+++ src/app/components/maps/maps.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class MapsComponent implements OnInit {
 
   public title = 'Property Solutions, sala 606';
-  public lat = -8.046716;
-  public lng = -34.8919388;
+  public lat = -8.12269631;
+  public lng = -34.90423501;
 
 
   constructor() { }
