2f213136135725ae4e5cae569596f80ad0be7952
onnion <marcio.hsdl@gmail.com>
2019-04-04 14:43:18 -0300

    [master] | contact integration

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,68 +1,64 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { FormBuilderValidators } from 'app/validators';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { FormBuilderValidators } from "app/validators";
+import { ContactService } from "app/services/contact/contact.service";
 
 @Component({
-  selector: 'app-contact',
-  templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  selector: "app-contact",
+  templateUrl: "./contact.component.html",
+  styleUrls: ["./contact.component.scss"]
 })
 export class ContactComponent implements OnInit {
-
   public form: FormGroup;
   public status_form = {
     loading: false,
     done: false
-
   };
 
-
-  constructor(private fb: FormBuilder, private custonValidator: FormBuilderValidators) { }
-
+  constructor(
+    private fb: FormBuilder,
+    private contactService: ContactService,
+    private custonValidator: FormBuilderValidators
+  ) {}
 
   private initFormControls(): void {
     this.form = this.fb.group({
-      email: ['', [Validators.required, this.custonValidator.emailFormat]],
-      name: ['', [Validators.required, this.custonValidator.nameFormat]],
-      phone: ['', [Validators.required, this.custonValidator.phoneFormat]],
-      message: ['', [Validators.required]]
+      email: ["", [Validators.required, this.custonValidator.emailFormat]],
+      name: ["", [Validators.required, this.custonValidator.nameFormat]],
+      phone: ["", [Validators.required, this.custonValidator.phoneFormat]],
+      message: ["", [Validators.required]]
     });
   }
 
-
   public getLabel(): string {
-    let label = 'Enviar';
+    let label = "Enviar";
 
     if (this.status_form.loading) {
-      label = 'Enviando';
+      label = "Enviando";
     }
 
     if (this.status_form.done) {
-      label = 'Enviado';
+      label = "Enviado";
     }
 
     return label;
-
   }
-
 
   public submit(): void {
     if (this.form.valid) {
       this.status_form.loading = true;
       this.form.disable();
-
-      setTimeout(() => {
-
-        this.status_form.done = true;
-        this.status_form.loading = false;
-
-      }, 2000);
+      this.contactService.sendContact(this.form.value).subscribe(
+        () => {
+          this.status_form.done = true;
+          this.status_form.loading = false;
+        },
+        error => console.log(error)
+      );
     }
   }
-
 
   ngOnInit() {
     this.initFormControls();
   }
-
 }
